e82d19c469e23b322693a7c104d65c3c859785c1
ZyCromerZ <neetroid97@gmail.com>
2020-08-30 12:24:48 +0700

    kernel: fix compiler string
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- kernel.sh
+++ kernel.sh
@@ -155,7 +155,7 @@ exports() {
 	export ARCH=arm64
 	export SUBARCH=arm64
 
-	KBUILD_COMPILER_STRING=$("$GCC64_DIR"/bin/aarch64-elf-gcc --version | head -n 1)
+	KBUILD_COMPILER_STRING=$("$CLANG_DIR"/bin/clang --version | head -n 1)
 	PATH=$CLANG_DIR/bin:$GCC64_DIR/bin/:$GCC32_DIR/bin/:/usr/bin:$PATH
 
 	export PATH KBUILD_COMPILER_STRING
